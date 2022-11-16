fd04c44436de6958bd71e9affd6813cb60a794b9
Olumide Micheal <olumidemicheal7@gmail.com>
2022-11-16 14:14:44 +0100

    done with the conversion section�

--- src/script.js
+++ src/script.js
@@ -1,2 +1,9 @@
-var afg = document.querySelector('.ngn');
+var afg = document.querySelector('.ngn').value;
 console.log(afg.innerHTML)
+
+
+if (afg.value == 'NGN - Nigerian Naira') {
+    console.log('yay!')
+} else {
+    console.log('Oh no!')
+}
