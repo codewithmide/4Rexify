7c8cae5b8279fa0a775f71674f817e39b2c3f44a
Olumide Micheal <olumidemicheal7@gmail.com>
2022-12-07 13:29:14 +0100

    cleaning the code

--- public/scripts/converter.js
+++ public/scripts/converter.js
@@ -1,68 +1,3 @@
-// let firstValue = document.getElementById('firstValue').value;
-// let firstCurrency = document.getElementById('currencyOne');
-// let secondCurrency = document.getElementById('currencyTwo');
-// let secondValue = document.getElementById('secondValue');
-// let btn = document.getElementById('convertBtnTwo')
-
-// // GET FIRST CURRENCY
-// function getcurrencyOne() {
-//   var selected = [];
-//   for (var option of document.getElementById('currencyOne').options) {
-//     if (option.selected) {
-//       selected.push(option.value);
-//     }
-//   }
-//   let one = selected.join('').toString()
-//   return one;
-// }
-// let currencyOne = '';
-// currencyOne = getcurrencyOne().toString();
-
-
-
-// // GET SECOND CURRENCY
-// function getcurrencyTwo() {
-//   var selected = [];
-//   for (var option of document.getElementById('currencyTwo').options) {
-//     if (option.selected) {
-//       selected.push(option.value);
-//     }
-//   }
-//   let two = selected.join('').toString()
-//   return two;
-// }
-// let currencyTwo = '';
-// currencyTwo = getcurrencyTwo().toString();
-
-
-// // VALUE TO BE CONVERTED
-// let valueOne = parseInt(firstValue);
-
-
-// // function getValueTwo() {
-// //   // FRANKFURTER API
-
-// //   let newvalueTwo = parseInt(currencyTwo);
-// //   return newvalueTwo;
-// // }
-
-// // getValueTwo();
-// // let valueTwo = parseInt(getValueTwo);
-
-// // btn.addEventListener('click', () => {
-//   const host = 'api.frankfurter.app';
-//   let newCurrency = [];
-//   newCurrency = getcurrencyTwo().toString();
-//   fetch(`https://${host}/latest?amount=${valueOne}&from=${currencyOne}&to=${currencyTwo}`)
-//   .then(resp => resp.json())
-//   .then((data) => {
-//     let newest = data.rates[0];
-//     let lastestCurrency = newest;
-//     console.log(data)
-//     console.log(`10 ${currencyOne} = ${lastestCurrency} ${currencyTwo}`);
-//   });
-// //})
-
 const convert = document.getElementById("convert");
 const result = document.getElementById("result");
 const valueOne = document.getElementById("valueOne");
@@ -83,7 +18,5 @@ convert.addEventListener("click", function() {
       let total = rate * amt;
       result.innerHTML = `${amt} ${fromCurrency} = ${total}
       ${toCurrency}`;
-      // valueOne.innerHTML = `${toCurrency}`
-      // valueTwo.innerHTML = `${fromCurrency}`
     });
 });
